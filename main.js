16b1067043d56c053b0426dc980c835386b36db5
Jong Wook Kim <jongwook@nyu.edu>
2017-09-18 03:47:14 -0400

    blinking arrow

--- main.js
+++ main.js
@@ -7,4 +7,11 @@
   var adjust = function() { $("#cover-image").css("height", $(window).height() + "px"); };
   $(window).on('resize', adjust);
   adjust();
+  window.onscroll = function () {
+    if (document.body.scrollTop > 0) {
+      $("#cover-title-bottom").hide();
+    } else {
+      $("#cover-title-bottom").show();
+    }
+  };
 }(document);
